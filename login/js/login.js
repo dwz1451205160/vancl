f9fe2e731a588c4f18ecbefdd52ce49bcfcc9a5a
chenyanjia <645799987@qq.com>
2018-12-10 11:47:23 +0800

    登录3-2

--- login/js/login.js
+++ login/js/login.js
@@ -23,4 +23,22 @@ $("#tags").find("li").eq(1).on("click",function(){
     $("#YZM2").css("display","block");
     $(".look").css("display","block");
     $("#phone-yzm").css("display","block");
+    $(".passError").css("display","none");
+    $(".userError").css("display","none");
+    $("#gantanhao1").css("display","none");
+    $("#gantanhao2").css("display","none");
+})
+$("#formInp").find("input").eq(0).on("blur",function(){
+    if(this.value == '') {
+        $(".userError").css("display","block");
+        $("#gantanhao1").css("display","block");
+        $(".userError").html("&ensp;&ensp;&ensp;&ensp;&ensp;用户名不能为空")
+    }
+})
+$("#formInp").find("input").eq(1).on("blur",function(){
+    if(this.value == '') {
+        $(".passError").css("display","block");
+        $("#gantanhao2").css("display","block");
+        $(".passError").html("&ensp;&ensp;&ensp;&ensp;&ensp;密码不能为空")
+    }
 })
